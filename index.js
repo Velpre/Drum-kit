2c67aca93ff7a2d4e3d71701648dd79304f57b8e
Velpre <65472724+Velpre@users.noreply.github.com>
2020-12-20 02:41:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+// finner ut hvilken knapp ble trykketpå
+for (var i = 0; i <= 6; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var button = this.innerHTML;
+    makeSound(button);
+    animation(button);
+  });
+};
+// finner ut hvuilken tastatur tast ble trykket på
+document.addEventListener("keydown", function (event){
+  var key = event.key
+  makeSound(key);
+  animation(key);
+  });
+
+function makeSound(key){
+  key = key.toLowerCase();
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var bass = new Audio("sounds/kick-bass.mp3");
+      bass.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+  }
+}
+
+function animation(key){
+  key = key.toLowerCase();
+  document.querySelector("."+key).classList.add("pressed");
+
+  setTimeout(function(){
+    document.querySelector("."+key).classList.remove("pressed");
+  }, 100);
+}
